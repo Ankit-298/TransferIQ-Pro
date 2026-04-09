f7ec11882df881d9b3d7fff1678971714a3adef8
Ankit-298 <ankit29086@gmail.com>
2026-04-09 19:33:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import os
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
-os.environ['TF_FORCE_GPU_ALLOW_GROWTH'] = 'true'
+os.environ['TF_USE_LEGACY_KERAS'] = '0'
 
 from flask import Flask, request, jsonify, send_file
 from flask_cors import CORS
